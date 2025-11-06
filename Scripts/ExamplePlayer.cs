ba466c20abeaf2d6e3ce2a74e00ed46dac42313b
ismith3-art <ismith3@ftstudent.org>
2025-11-06 12:13:33 -0500

    finished flashlight

--- Scripts/ExamplePlayer.cs
+++ Scripts/ExamplePlayer.cs
@@ -7,10 +7,12 @@ public partial class ExamplePlayer : CharacterBody2D
 	public float Speed = 300.0f;
 
 	private AnimatedSprite2D sprite;
+	private PointLight2D flashlight;
 
 	public override void _Ready()
 	{
 		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		flashlight = GetNode<PointLight2D>("PointLight2D");
 	}
 	
 	public override void _PhysicsProcess(double delta)
@@ -24,14 +26,21 @@ public partial class ExamplePlayer : CharacterBody2D
 		{
 			velocity = direction * Speed;
 			sprite.FlipH = velocity.X < 0;
+			
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
-
 		Velocity = velocity;
 		MoveAndSlide();
+		Rotation = Velocity.Angle() + Mathf.DegToRad(0);
+		if (Velocity == Vector2.Zero) {
+			flashlight.Visible = false;
+			}
+		else {
+			flashlight.Visible = true;
+			}
 	}
 }
