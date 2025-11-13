b5cd1e690deb0c404a422859c08a79a15d7cd97b
Cbailey10! <crcbailey10@gmail.com>
2025-11-13 11:54:34 -0500

    Added flashlight and code Fixed map

--- Scripts/ExamplePlayer.cs
+++ Scripts/ExamplePlayer.cs
@@ -1,4 +1,4 @@
- using Godot;
+using Godot;
 using System;
 
 public partial class ExamplePlayer : CharacterBody2D
@@ -19,32 +19,28 @@ public partial class ExamplePlayer : CharacterBody2D
 	{
 		var velocity = Velocity;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity = direction * Speed;
 			sprite.FlipH = velocity.X < 0;
-			
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+			velocity = Vector2.Zero;
 		}
+
 		Velocity = velocity;
 		MoveAndSlide();
 		Rotation = Velocity.Angle() + Mathf.DegToRad(0);
-		if (Velocity == Vector2.Zero) {
+
+		if (Velocity == Vector2.Zero) 
+		{
 			flashlight.Visible = false;
-			}
-		else {
+		}
+		else 
+		{
 			flashlight.Visible = true;
-			}
-<<<<<<< Updated upstream
-=======
-		
->>>>>>> Stashed changes
+		}
 	}
 }
