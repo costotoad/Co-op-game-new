e0a8da34964dd8f310105fdba14bee4012d5e7f0
costotoad <vkruger@ftstudent.org>
2025-11-19 12:18:49 -0500

    Added Bo's stuff

--- Scripts/ExamplePlayer.cs
+++ Scripts/ExamplePlayer.cs
@@ -1,4 +1,4 @@
- using Godot;
+using Godot;
 using System;
 
 public partial class ExamplePlayer : CharacterBody2D
@@ -7,18 +7,18 @@ public partial class ExamplePlayer : CharacterBody2D
 	public float Speed = 300.0f;
 
 	private AnimatedSprite2D sprite;
+	private PointLight2D flashlight;
 
 	public override void _Ready()
 	{
 		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		flashlight = GetNode<PointLight2D>("PointLight2D");
 	}
 	
 	public override void _PhysicsProcess(double delta)
 	{
 		var velocity = Velocity;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
@@ -27,11 +27,20 @@ public partial class ExamplePlayer : CharacterBody2D
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+			velocity = Vector2.Zero;
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
+		Rotation = Velocity.Angle() + Mathf.DegToRad(0);
+
+		if (Velocity == Vector2.Zero) 
+		{
+			flashlight.Visible = false;
+		}
+		else 
+		{
+			flashlight.Visible = true;
+		}
 	}
 }
