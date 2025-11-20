1d3d4366ebecbb16f6b2ba2fced0f143d6722932
Cbailey10! <crcbailey10@gmail.com>
2025-11-20 12:35:58 -0500

    code works colors are hard to tell right now

--- Scripts/ExamplePlayer.cs
+++ Scripts/ExamplePlayer.cs
@@ -1,10 +1,11 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody2D
+public partial class ExamplePlayer : CharacterBody2D
 {
-	[Export]
-	public float Speed = 300.0f;
+	[Export] public int PlayerID = 0;
+	[Export] public float Speed = 300.0f;
+
 	private AnimatedSprite2D sprite;
 	private PointLight2D flashlight;
 
@@ -12,12 +13,20 @@ public partial class Player : CharacterBody2D
 	{
 		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		flashlight = GetNode<PointLight2D>("PointLight2D");
+		flashlight.Color=GetRandomColor();
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		var velocity = Velocity;
-		var direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		float x = Input.GetJoyAxis(PlayerID, JoyAxis.LeftX);
+		float y = Input.GetJoyAxis(PlayerID, JoyAxis.LeftY);
+
+		if (Mathf.Abs(x) < 0.2f) x = 0;
+		if (Mathf.Abs(y) < 0.2f) y = 0;
+
+		Vector2 direction = new Vector2(x, y);
+
+		Vector2 velocity = Velocity;
 
 		if (direction != Vector2.Zero)
 		{
@@ -32,6 +41,19 @@ public partial class Player : CharacterBody2D
 		Velocity = velocity;
 		MoveAndSlide();
 		Rotation = Velocity.Angle();
-		flashlight.Visible = Velocity != Vector2.Zero;
+
+		if (Velocity == Vector2.Zero)
+			flashlight.Visible = false;
+		else
+			flashlight.Visible = true;
+	}
+	
+	public Color GetRandomColor(){
+		float r = GD.Randf();
+		float g = GD.Randf();
+		float b = GD.Randf();
+		float a = 5;
+		
+		return new Color(r, g, b, a);
 	}
 }
