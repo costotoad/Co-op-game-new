49fedadb32a9a298ba6ff46c2c1dcbb9990a8ee0
ismith3-art <ismith3@ftstudent.org>
2025-11-20 12:24:16 -0500

    Revert "Flashlight"

--- Scripts/ExamplePlayer.cs
+++ Scripts/ExamplePlayer.cs
@@ -1,24 +1,25 @@
-using Godot;
+ using Godot;
 using System;
 
-public partial class Player : CharacterBody2D
+public partial class ExamplePlayer : CharacterBody2D
 {
 	[Export]
 	public float Speed = 300.0f;
+
 	private AnimatedSprite2D sprite;
-	private PointLight2D flashlight;
 
 	public override void _Ready()
 	{
 		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		flashlight = GetNode<PointLight2D>("PointLight2D");
 	}
-
+	
 	public override void _PhysicsProcess(double delta)
 	{
 		var velocity = Velocity;
-		var direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity = direction * Speed;
@@ -26,12 +27,11 @@ public partial class Player : CharacterBody2D
 		}
 		else
 		{
-			velocity = Vector2.Zero;
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
-		Rotation = Velocity.Angle();
-		flashlight.Visible = Velocity != Vector2.Zero;
 	}
 }
