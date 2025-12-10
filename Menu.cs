9a62197a10148438396bdb9d07872b65c3cf7347
ismith3-art <ismith3@ftstudent.org>
2025-12-10 11:19:02 -0500

    Menu as its own Scene

--- Menu.cs
+++ Menu.cs
@@ -1,12 +1,13 @@
 using Godot;
 using System;
-public partial class Menu : Node2D
+public partial class Menu : Node
 {
 	private Sprite2D titleScreen;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		titleScreen=GetNode<Sprite2D>("HighkeyTrashTitleScreen") as Sprite2D;
+		GetTree().Paused=true;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,6 +23,8 @@ public partial class Menu : Node2D
 	{
 		GD.Print("Pressed");
 		titleScreen.Visible=false;
+		GetTree().Paused=false;
+		QueueFree();
 	}
 		
 		
